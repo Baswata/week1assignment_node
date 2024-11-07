d3232e87b84dfea9af67896b43725ad367d3e3d7
Belinda Aswata <aswatabelinda@gmail.com>
2024-11-07 12:52:55 +0300

    lesson 8- event emitters

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 console.log("Hello, Node.js");
+
 const http = require('http');
 const PORT = 3000;
-const handleRequest = require('./requestHandler');
 const axios = require('axios');
+const EventEmitter = require('events');
+const handleRequest = require('./requestHandler'); 
 
+
+const serveEmitter = new EventEmitter();
+
+serveEmitter.on('requestReceived', (method, url) => {
+    console.log(`Request received: ${method} ${url}`);
+});
+
+
+const server = http.createServer((req, res) => {
+    serveEmitter.emit('requestReceived', req.method, req.url);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+});
+
+// Fetch data using async/await and handle errors
 async function fetchData() {
-    try{
+    try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
         console.log(response.data);
-
-    } catch( error) {
+    } catch (error) {
         if (error.response) {
-            console.error(`Error: ${error.response.ststus} - ${error.response.statusText}`);
-
-        }else if (error.request) {
+            console.error(`Error: ${error.response.status} - ${error.response.statusText}`);
+        } else if (error.request) {
             console.error(`Error: No response received from API`);
         } else {
             console.error('Error:', error.message);
@@ -23,8 +38,6 @@ async function fetchData() {
 
 fetchData();
 
-const server = http.createServer(handleRequest); 
-
 server.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
