549aeb2ce108b6a0d1db73dbab336d176f385b92
Belinda Aswata <aswatabelinda@gmail.com>
2024-11-07 10:33:53 +0300

    lesson 6 done

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ console.log("Hello, Node.js");
 const http = require('http');
 const PORT = 3000;
 const handleRequest = require('./requestHandler');
+const axios = require('axios');
+
+async function fetchData() {
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        console.log(response.data);
+
+    } catch( error) {
+        console.error('Error fetching data', error);
+    }
+}
+
+fetchData();
 
 const server = http.createServer(handleRequest); 
 
