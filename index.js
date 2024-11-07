46e0414f76966ae5414adaf943c7d48a968b378a
Belinda Aswata <aswatabelinda@gmail.com>
2024-11-07 09:46:08 +0300

    lesson 4 done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, Node.js")
+console.log("Hello, Node.js");
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!')
+});
+
+server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`)
+})
